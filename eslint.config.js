af65f4533807606a0e659e1577edb0d275d58be1
YokoyamaGen <g.yokoyama.0616@gmail.com>
2026-05-04 14:54:10 +0900

    TypeScript ESLint pluginを追加してlintルールを強化

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import tsEslintParser from "@typescript-eslint/parser";
+import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
 import playwright from "eslint-plugin-playwright";
 
 export default [
@@ -10,9 +11,13 @@ export default [
         project: "./tsconfig.json",
       },
     },
-    plugins: { playwright },
+    plugins: { playwright, "@typescript-eslint": tsEslintPlugin },
     rules: {
       ...playwright.configs.recommended.rules,
+      "no-console": "error",
+      "@typescript-eslint/no-unused-vars": ["error", { vars: "all", args: "after-used", ignoreRestSiblings: true }],
+      "no-unreachable": "error",
+      "no-unreachable-loop": "error",
     },
   },
 ]
